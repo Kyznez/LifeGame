36008930ea857703c97678a05bd1c2294171d215
Kyznez Vakyla <kyznezvakyla@mail.ru>
2020-03-11 01:03:01 +0500

    Main window

--- Display.java
+++ Display.java
@@ -1,1 +1,10 @@
 public abstract class Display{
+    private static boolean created = false;
+    private static JFrame window;
+    private static Canvas content;
+    public static void create(int width, int height, String title){
+
+    }
+
+}
+
