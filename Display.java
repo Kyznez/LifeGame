8abc9aa544598b07ec979fd50a2104714ec35295
Kyznez <kyznezvakyla@mail.ru>
2020-03-11 20:14:21 +0500

    Checker

--- Display.java
+++ Display.java
@@ -3,7 +3,9 @@ public abstract class Display{
     private static JFrame window;
     private static Canvas content;
     public static void create(int width, int height, String title){
-
+        if (created)
+            return;
+        window = new JFrame(title);
     }
 
 }
