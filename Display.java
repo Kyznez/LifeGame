bf45109cc59c76d59f8beb0f363da5aaad567832
Kyznez <kyznezvakyla@mail.ru>
2020-03-20 19:38:40 +0500

    0.2

--- Display.java
+++ Display.java
@@ -8,12 +8,16 @@ public abstract class Display{
         if (created)
             return;
         window = new JFrame(title);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         content = new Canvas();
         Dimension size = new Dimension(width, height);
         content.setPreferredSize(size);
         window.setResizable(false);
         window.getContentPane().add(content);
         window.pack();
+
+        window.setVisible(true);
+
 
 
 
